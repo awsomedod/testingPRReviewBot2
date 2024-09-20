d2675554fe0650b59ac6ddf94246dab22f004d93
awsomedod <awsomedod@gmail.com>
2024-09-20 13:57:42 -0700

    Initial project setup with home page and database configuration

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from models import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
